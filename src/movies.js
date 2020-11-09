692a4ec08e2daecb77ba27c3b53d5ff6142ebeae
AlvaroSL <alvaro.sanchez.lamadrid@gmail.com>
2020-11-09 18:41:11 +0100

    small changes in the lab

--- src/movies.js
+++ src/movies.js
@@ -1,81 +1,100 @@
 // Iteration 1: All directors? - Get the array of all directors.
 
+function getAllDirectors(movieArray){
+    let directors = movieArray.map((elem) => {
+        return elem.director
+    })
+    return directors
+}
 
-
-
-
-
-
-// {function getAllDirectors(movieArray) {
-//     let directors = movieArray.map(function(item){
-//         return item.director;
-//     })
-// }}
-    
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
-// function howManyMovies(movieArray){
-//     let filteredList = movieArray.filter(function(item){
-//         return item.["director"] === "Steven Spielberg" && item.["genre"].includes("Drama")
-//     })
-//     return filteredList.length;
-// }
+function howManyMovies(movieArray){ 
+    if (movieArray.length === 0){
+        return 0
+    }
+    let movieSteven = movieArray.filter((elem) =>{
+       return elem.director === "Steven Spielberg" && elem.genre.includes("Drama")
+    })
+    return movieSteven.length;
+}
 
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
-// function ratesAverage(movieArray) {
-//     if (movies.length == 0){
-//         return 0;
-//     }
+function ratesAverage(movieArray){
+    if (movieArray.length === 0){
+        return 0;
+    }
 
+    let average = movieArray.reduce((total, acc) =>{
+        return total + acc['rate']/movieArray.length
+    }, 0)
 
-//     let ratesAverage = movieArray.reduce(function(acc, item){
-//         return acc + item.["rate"] / movieArray.length
-//     }, 0)
-//     return ratesAverage.toFixed(2)
-// }
-
+    return Number(average.toFixed(2))
+}
 
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
-// function dramaMoviesRate(movieArray) {
-//     let dramaMovies = movieArray.filter(function(item){
-//         return item.["genre"].["Drama"]
-//     })
-//     return ratesAverage(dramaMovies)
-// }
+function dramaMoviesRate(movieArray){
+    let averageDrama = movieArray.filter((elem) => {
+        return elem.genre.includes("Drama")
+    })
+
+    return ratesAverage(averageDrama);
+}
 
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
-// function orderByYear(movieArray) {
-//     let cloneArray = JSON.parse(JSON.stringify(movieArray))
+function orderByYear(movieArray){
+    let cloneArray = JSON.parse(JSON.stringify(movieArray))
+    let sortedArr = cloneArray.sort((a, b) =>{
+        if (a.year > b.year) {
+            return 1;
+          } else if (b.year > a.year) {
+            return -1;
+          } else {
+            if (a.title > b.title) {
+              return 1;
+            } else if (b.title > a.title) {
+              return -1;
+            }
+            return 0;
+          }
+    })
 
-//     cloneArray.sort(function(a,b){
-//         if (a.year > b.year) {
-//             return 1;
-//         } else if (a.year < b.year) {
-//             return -1;
-//         } else {
-//             if (a.title > b.title) {
-//                 return 1;
-//             } else if (a.title < b.title) {
-//                 return -1;
-//             } else {
-//                 return 0;
-//             }
-//         }
-//     })
+    return sortedArr
+}
 
-//     return cloneArray
-// }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(movieArray){
+    let cloneArr = JSON.parse(JSON.stringify(movieArray));
+
+    let sortAlfabetically = cloneArr.sort((a, b) => {
+        if (a.title > b.title) {
+          return 1;
+        } else if (a.title < b.title) {
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+
+    let twentyArr = sortAlfabetically.map((elem) => {
+        return elem.title
+    }).slice(0, 20)
+
+   return twentyArr
+
+}
+
 
 // function orderAlphabetically(movieArray) {
 //     let cloneAlphabetically = JSON.parse(JSON.stringify(movieArray))
